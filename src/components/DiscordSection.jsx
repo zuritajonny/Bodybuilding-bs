8d90fbc5f72390b3cacaedb95c7fc483bdce7633
zuritajonny <jonnyzuritadev@gmail.com>
2022-11-29 12:35:09 -0400

    Discord Icon added and new copy

--- src/components/DiscordSection.jsx
+++ src/components/DiscordSection.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { FiArrowRight } from "react-icons/fi";
+import discord from "../media/Icons/discordIcon.svg";
 
 function DiscordSection() {
   return (
     <section className="discord">
-      <h2 className="big-text">Join our Discord server!</h2>
-      <h5 className="big-text--subtitle">
-        A place for discussion, learning and getting better.
-      </h5>
+      <img src={discord} alt="" />
+      <h2 className="big-text">Ready to join the Srs Discord?</h2>
+      <h5 className="big-text--subtitle">A very srs community...</h5>
       <p className="hero-paragraph paragraph-text">
-        Learn from other people with likeminded goals and become your better and
-        bigger version. Everyone is welcome.
-        <br />
-        We are building more than a fitness community! Tune in for more if that
-        interests you srs
+        If you are looking to join an army of srs lads and chads you've found
+        the spot. Epic ments and legendary gents helping each other ascend to
+        unknown heights. You don't want to miss out... very srs,,,
       </p>
 
       <a href="https://discord.gg/srsbodybuild" target="_blank">
