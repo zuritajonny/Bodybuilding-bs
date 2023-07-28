343a63c51cbf8b05d7589b04ee4dda2df7ab945a
zuritajonny <jonnyzuritadev@gmail.com>
2023-07-28 16:36:06 -0400

    Latest Changes

--- src/components/InstagramWidget.jsx
+++ src/components/InstagramWidget.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { ElfsightWidget } from "react-elfsight-widget";
 
 function InstagramWidget() {
+  window.addEventListener("load", (event) => {
+    const Message = document.querySelector(
+      ".elfsight-app-f0e3a281-00e6-490e-8d3d-9bfa866b7f78 > a"
+    );
+    console.log(Message);
+  });
   return (
     <div className="IG--block__wrapper">
       <ElfsightWidget widgetID="f0e3a281-00e6-490e-8d3d-9bfa866b7f78" />
