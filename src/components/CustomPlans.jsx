b03b39595f4986afa7e221429740aeb9e76a8bcb
Luis Landaeta <luis.landaeta24@gmail.com>
2023-08-01 16:51:51 -0400

    added Affirm buttons to custom plans

--- src/components/CustomPlans.jsx
+++ src/components/CustomPlans.jsx
@@ -2,6 +2,86 @@ import React from "react";
 import AffirmLogo from "../media/affirm-logo.webp";
 
 function CustomPlans() {
+  function HandleAffirmButton(name, price) {
+    affirm.checkout({
+      merchant: {
+        user_confirmation_url:
+          "http://development.bodybuildingandbs.com/checkout",
+        user_cancel_url: "http://development.bodybuildingandbs.com/canceled",
+        user_confirmation_url_action: "POST",
+        name: "Bodybuilding & BS",
+      },
+      shipping: {
+        name: {
+          first: "Joe",
+          last: "Doe",
+        },
+        address: {
+          line1: "633 Folsom St",
+          line2: "Floor 7",
+          city: "San Francisco",
+          state: "CA",
+          zipcode: "94107",
+          country: "USA",
+        },
+        phone_number: "4153334567",
+        email: "joedoe@123fakestreet.com",
+      },
+      billing: {
+        name: {
+          first: "Joe",
+          last: "Doe",
+        },
+        address: {
+          line1: "633 Folsom St",
+          line2: "Floor 7",
+          city: "San Francisco",
+          state: "CA",
+          zipcode: "94107",
+          country: "USA",
+        },
+        phone_number: "4153334567",
+        email: "joedoe@123fakestreet.com",
+      },
+      items: [
+        {
+          display_name: name,
+          sku: "ABC-123",
+          unit_price: price,
+          qty: 1,
+          // item_image_url: "http://merchantsite.com/images/awesome-pants.jpg",
+          // item_url: "http://merchantsite.com/products/awesome-pants.html",
+          // categories: [
+          //   ["Home", "Bedroom"],
+          //   ["Home", "Furniture", "Bed"],
+          // ],
+        },
+      ],
+      // discounts: {
+      //   RETURN5: {
+      //     discount_amount: 500,
+      //     discount_display_name: "Returning customer 5% discount",
+      //   },
+      //   PRESDAY10: {
+      //     discount_amount: 1000,
+      //     discount_display_name: "President's Day 10% off",
+      //   },
+      // },
+      /*   metadata: {
+        shipping_type: "UPS Ground",
+        mode: "modal",
+      }, */
+      order_id: "JKLMO4321",
+      currency: "USD",
+      financing_program: "flyus_3z6r12r",
+      shipping_amount: 0,
+      tax_amount: 0,
+      total: price,
+    });
+
+    affirm.checkout.open();
+  }
+
   return (
     <div className="plans--container plans--container__custom mn">
       <h3 className="heading text--white mn--title">
@@ -26,9 +106,20 @@ function CustomPlans() {
             <li>Fast Delivery</li>
           </ul>
           <div className="buttons--wrapper buttons--wrapper__horizontal">
-            <a href="" className="button button--affirm">
-              Pay with
-              <img src={AffirmLogo} alt="Affirm Logo" className="img--afirm" />
+            <a className="purchase-button-link">
+              <button
+                className="button button--affirm"
+                onClick={() =>
+                  HandleAffirmButton("3-month bulking plan", 49900)
+                }
+              >
+                Pay with
+                <img
+                  src={AffirmLogo}
+                  alt="Affirm Logo"
+                  className="img--afirm"
+                />
+              </button>
             </a>
             <a
               href="https://payments.bodybuildingandbs.com/3-months-bulking-plan"
@@ -55,9 +146,20 @@ function CustomPlans() {
             <li>Fast Delivery</li>
           </ul>
           <div className="buttons--wrapper buttons--wrapper__horizontal">
-            <a href="" className="button button--affirm">
-              Pay with
-              <img src={AffirmLogo} alt="Affirm Logo" className="img--afirm" />
+            <a className="purchase-button-link">
+              <button
+                className="button button--affirm"
+                onClick={() =>
+                  HandleAffirmButton("3-month shredding plan", 49900)
+                }
+              >
+                Pay with
+                <img
+                  src={AffirmLogo}
+                  alt="Affirm Logo"
+                  className="img--afirm"
+                />
+              </button>
             </a>
             <a
               href="https://payments.bodybuildingandbs.com/3-months-sherdding-plan"
@@ -83,9 +185,20 @@ function CustomPlans() {
             <li>Fast Delivery</li>
           </ul>
           <div className="buttons--wrapper buttons--wrapper__horizontal">
-            <a href="" className="button button--affirm">
-              Pay with
-              <img src={AffirmLogo} alt="Affirm Logo" className="img--afirm" />
+            <a className="purchase-button-link">
+              <button
+                className="button button--affirm"
+                onClick={() =>
+                  HandleAffirmButton("3-month body recomposition plan", 49900)
+                }
+              >
+                Pay with
+                <img
+                  src={AffirmLogo}
+                  alt="Affirm Logo"
+                  className="img--afirm"
+                />
+              </button>
             </a>
             <a
               href="https://payments.bodybuildingandbs.com/3-months-body-recomposition-plan"
@@ -111,9 +224,20 @@ function CustomPlans() {
             <li>Fast Delivery</li>
           </ul>
           <div className="buttons--wrapper buttons--wrapper__horizontal">
-            <a href="" className="button button--affirm">
-              Pay with
-              <img src={AffirmLogo} alt="Affirm Logo" className="img--afirm" />
+            <a className="purchase-button-link">
+              <button
+                className="button button--affirm"
+                onClick={() => {
+                  HandleAffirmButton("6-week reverse diet plan", 29900);
+                }}
+              >
+                Pay with
+                <img
+                  src={AffirmLogo}
+                  alt="Affirm Logo"
+                  className="img--afirm"
+                />
+              </button>
             </a>
             <a
               href="https://payments.bodybuildingandbs.com/6-week-revers-diet-plan"
